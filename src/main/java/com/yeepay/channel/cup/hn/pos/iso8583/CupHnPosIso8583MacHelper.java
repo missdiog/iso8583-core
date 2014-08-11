5e6153c82c88bc3f88de89c5618a939ec9dfc9f0
wangliangbo <wlb13041064>
2014-08-11 14:28:42 +0800

    MAC辅助类修改

--- src/main/java/com/yeepay/channel/cup/hn/pos/iso8583/CupHnPosIso8583MacHelper.java
+++ src/main/java/com/yeepay/channel/cup/hn/pos/iso8583/CupHnPosIso8583MacHelper.java
@@ -1,13 +1,19 @@
 package com.yeepay.channel.cup.hn.pos.iso8583;
 
+import java.util.Arrays;
+
 import me.andpay.ti.base.AppBizException;
 import me.andpay.ti.lnk.annotaion.Lnkwired;
 import me.andpay.ti.s3.api.crypto.TxnAppCryptoService;
+import me.andpay.ti.s3.api.kms.KeyDataWithCv;
+import me.andpay.ti.util.ByteUtil;
+import me.andpay.ti.util.HexUtil;
 
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISOPackager;
 
 import com.yeepay.message.iso8583.Iso8583MacHelper;
+import com.yeepay.message.iso8583.Iso8583Operator;
 import com.yeepay.message.iso8583.Iso8583StandardFieldNoes;
 
 /**
@@ -34,7 +40,6 @@ public class CupHnPosIso8583MacHelper implements Iso8583MacHelper {
 	 * {@inheritDoc}
 	 */
 	public byte[] calcMac(ISOMsg isoMsg, String macKey) throws AppBizException {
-		/*
 		byte[] macStub = new byte[8];
 
 		if (isoMsg.hasField(macFieldNo) == false) {
@@ -57,6 +62,7 @@ public class CupHnPosIso8583MacHelper implements Iso8583MacHelper {
 			mab = ByteUtil.rightPad(mab, mab.length + 8 - mab.length % 8, (byte) 0x00);
 		}
 
+		/*
 		// ECB XOR
 		byte[] mabUnit = ByteUtil.getBytes(mab, 0, 8);
 		for (int offset = 8; offset < mab.length; offset += 8) {
@@ -66,7 +72,48 @@ public class CupHnPosIso8583MacHelper implements Iso8583MacHelper {
 		String mabUnitHex = HexUtil.encodeHex(mabUnit);
 		byte[] mabUnitHigh = mabUnitHex.substring(0, 8).getBytes();
 		byte[] mabUnitLow = mabUnitHex.substring(8).getBytes();
-
+		*/
+		
+		// TEK
+		KeyDataWithCv tekByLmk = KeyDataWithCv.fromHex(macKey);
+		
+		//去除MAC域内容后，取前8字节进行DES
+		byte[] desMacUnit = txnAppCryptoService.encryptDataByTek(tekByLmk, ByteUtil.getBytes(mab, 0, 8), false);
+		
+		byte[] mabUnit = desMacUnit;
+		
+		for (int offset = 8; offset < mab.length; offset += 8) {
+			//计算结果后8字节进行XOR
+			mabUnit = ByteUtil.xor(mabUnit, ByteUtil.getBytes(mab, offset, 8));
+			// 用MAK对XOR结果进行DES
+			mabUnit = txnAppCryptoService.encryptDataByTek(tekByLmk, mabUnit, false);
+		}
+		
+		return mabUnit;
+		
+		/*
+		// 计算结果与HEX后8字节进行XOR
+		desMacUnit = ByteUtil.xor(desMacUnit, mabUnitLow);
+		// 用MAK对XOR结果进行DES
+		desMacUnit = txnAppCryptoService.encryptDataByTek(tekByLmk, desMacUnit, false);
+		// 取计算结果前4字节转成HEX
+		desMacUnit = ByteUtil.getBytes(desMacUnit, 0, 4);
+		// HEX后8个字符即为MAC
+		return HexUtil.encodeHex(desMacUnit).getBytes();
+		*/
+		
+		
+		/*
+		// ECB XOR
+		byte[] mabUnit = ByteUtil.getBytes(mab, 0, 8);
+		for (int offset = 8; offset < mab.length; offset += 8) {
+			mabUnit = ByteUtil.xor(mabUnit, ByteUtil.getBytes(mab, offset, 8));
+		}
+		// 将XOR结果转HEX
+		String mabUnitHex = HexUtil.encodeHex(mabUnit);
+		byte[] mabUnitHigh = mabUnitHex.substring(0, 8).getBytes();
+		byte[] mabUnitLow = mabUnitHex.substring(8).getBytes();
+		
 		// TEK
 		KeyDataWithCv tekByLmk = KeyDataWithCv.fromHex(macKey);
 		// 用MAK对HEX前8字节进行DES
@@ -78,22 +125,22 @@ public class CupHnPosIso8583MacHelper implements Iso8583MacHelper {
 		// 取计算结果前4字节转成HEX
 		desMacUnit = ByteUtil.getBytes(desMacUnit, 0, 4);
 		// HEX后8个字符即为MAC
-		return HexUtil.encodeHex(desMacUnit).getBytes();*/
-		return "00000000".getBytes();
+		return HexUtil.encodeHex(desMacUnit).getBytes();
+		*/
+		//return "00000000".getBytes();
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public void verifyMac(ISOMsg isoMsg, String macKey) throws AppBizException {
-		/*
 		byte[] actMac = Iso8583Operator.getFieldBinary(isoMsg, macFieldNo);
 		byte[] expMac = calcMac(isoMsg, macKey);
 		if (Arrays.equals(expMac, actMac) == false) {
 			// MAC校验错误
 			throw new RuntimeException(String.format("Mac verify error, expMac=%s, actMac=%s",
 					HexUtil.encodeHex(expMac), HexUtil.encodeHex(actMac)));
-		}*/
+		}
 	}
 
 	/**
